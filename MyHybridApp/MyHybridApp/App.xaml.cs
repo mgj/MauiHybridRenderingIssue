9cea8a407edee5291a4274196c470466ff0d9aad
mgj <mgj@mgjs.dk>
2024-10-23 11:19:57 +0200

    init

---
+++ MyHybridApp/MyHybridApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyHybridApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MyHybridApp/MyHybridApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MyHybridApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
